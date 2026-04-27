41cf42ca6fce9cd3be21ee779f927314fd4bf117
MJ <jogademayur@gmail.com>
2026-04-27 09:06:36 +0530

    new commit

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -1,18 +1,57 @@
+
+
 function addTask() {
-    let task = document.getElementById("task").value;
+    let input = document.getElementById("input");
+    let task = input.value;
 
     if (task === "") return;
 
-    let list = document.getElementById("list");
+    //create elements;
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    let editBtn = document.createElement("button");
+    let deleteBtn = document.createElement("button");
 
-    list.innerHTML += `
-        <li>
-            ${task}
-            <button onclick="deleteTask(this)">Delete</button>
-        </li>
-    `;
-}
+    //set content
+    span.textContent = task;
+    editBtn.textContent = "Edit"
+    deleteBtn.textContent = "Delete"
 
-function deleteTask(btn) {
-    btn.parentElement.remove();
+    //delete func
+    deleteBtn.onclick = function () {
+        li.remove();
+    }
+
+    //EDIT FUNC
+    editBtn.onclick = function () {
+        if (editBtn.textContent === "Edit") {
+            //create input box element
+
+            let inputBox = document.createElement("input");
+            inputBox.value = span.textContent;
+
+            li.replaceChild(inputBox, span);
+            editBtn.textContent = "Save";
+        } else {
+            //find the input box
+            let inputBox = li.querySelector("input");
+            let newText = inputBox.value;
+
+            if (newText === "") return;
+
+            span.textContent = newText;
+
+            li.replaceChild(span, inputBox);
+            editBtn.textContent = "Edit"
+        }
+    }
+    //append elements
+    li.appendChild(span);
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+
+    document.getElementById("list").appendChild(li);
+
+    input.value = "";
+
 }
